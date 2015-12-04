0916e8def6da1c1b64c9d492f231fa6ec584e815
vladuionut <vladuionut@yahoo.com>
2015-12-04 16:51:51 +0200

    css changes

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,4 +4,15 @@
 $(function(){
 
   $.material.init();
+  io.socket.on('connect', function(){
+    io.socket.get('/user',function(){
+      console.log(arguments);
+    });
+  });
+
+  io.socket.on('disconnect', function(){
+    console.log('Lost connection to server');
+  });
+
+
 });
